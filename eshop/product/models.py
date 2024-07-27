d3fe024e044fefa9cc584453a972a381ced256d5
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-07-27 06:40:28 +0600

    Update models.py

--- eshop/product/models.py
+++ eshop/product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.TextChoices): ##text choices used for dropdown
@@ -17,7 +18,12 @@ class Product(modesl.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     brand = models.CharField(max_length=200, default="", blank=False)
     category = models.CharField(max_length=30, choices=Category.choices) ## drop down categories will show
+    ratings = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    stock = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) #To create a relationship with the user. Set_null is used beacuse if the user is deleted don't delete the product just make the user null
+    createdAt = models.DateTimeField(auto_now_add=True) # new product will automatically create date and time
 
     
+
 
 
